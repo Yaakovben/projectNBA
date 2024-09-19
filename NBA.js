1b8c5ccadcb3795c45d28f2abef49ac732c74b13
Yaakovben <yaakovbenyizchak1@gmail.com>
2024-09-19 15:49:08 +0300

    I have done initial work on the ADD button in the table

--- NBA.js
+++ NBA.js
@@ -15,6 +15,12 @@ const threePercent = document.querySelector('.three');
 const points = document.querySelector('.points');
 const Search = document.querySelector('.Search');
 const Table = document.querySelector('.Table');
+//
+const Point = document.querySelector('.Point');
+const Shooting = document.querySelector('.Shooting');
+const Small = document.querySelector('.Small');
+const Power = document.querySelector('.Power');
+const Center = document.querySelector('.Center');
 const addPlayer = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const res = yield fetch(BASE_URL, {
@@ -42,23 +48,95 @@ const addPlayer = () => __awaiter(void 0, void 0, void 0, function* () {
 const displayPlayer = (player) => __awaiter(void 0, void 0, void 0, function* () {
     // יצירת שורה
     let newRow = document.createElement("tr");
+    newRow.classList.add("prm");
     // יצירת אברים שיוצגו בשורה
     let td1 = document.createElement("td");
     let td2 = document.createElement("td");
     let td3 = document.createElement("td");
     let td4 = document.createElement("td");
     let td5 = document.createElement("td");
-    let button = document.createComment("button");
+    let td6 = document.createElement("td");
+    let button = document.createElement("button");
+    button.classList.add("buttonDisplay");
+    button.addEventListener("click", () => displayOnCubse(player));
     td1.textContent = player.playerName;
     td2.textContent = player.position;
-    td3.textContent = player.twePercent;
-    td4.textContent = player.threePercent;
-    button.textContent = `add${player.playerName} to Current Team`;
+    td3.textContent = player.points.toString();
+    td4.textContent = player.twoPercent.toString();
+    td5.textContent = player.threePercent.toString();
+    button.textContent = `add "${player.playerName}" to Current Team`;
+    td6.appendChild(button);
     newRow.appendChild(td1);
     newRow.appendChild(td2);
     newRow.appendChild(td3);
     newRow.appendChild(td4);
+    newRow.appendChild(td5);
     newRow.appendChild(button);
     Table.appendChild(newRow);
 });
+//
 Search.addEventListener("click", addPlayer);
+const displayOnCubse = (player) => {
+    let title = document.createElement("h3");
+    title.style.color = "blue";
+    let name = document.createElement("h1");
+    let three = document.createElement("h3");
+    let two = document.createElement("h3");
+    let points = document.createElement("h3");
+    name.textContent = player.playerName;
+    three.textContent = ` Three Precents: ${player.threePercent.toString()}%`;
+    two.textContent = `Tow Precents: ${player.twoPercent.toString()}%`;
+    points.textContent = `points: ${player.points.toString()}`;
+    switch (player.position) {
+        case "PG": {
+            Point.innerHTML = "";
+            title.textContent = "Point Guard";
+            Point.appendChild(title);
+            Point.appendChild(name);
+            Point.appendChild(three);
+            Point.appendChild(two);
+            Point.appendChild(points);
+            break;
+        }
+        case "SG": {
+            Shooting.innerHTML = "";
+            title.textContent = "Shooting Guard";
+            Shooting.appendChild(title);
+            Shooting.appendChild(name);
+            Shooting.appendChild(three);
+            Shooting.appendChild(two);
+            Shooting.appendChild(points);
+            break;
+        }
+        case "SF": {
+            Small.innerHTML = "";
+            title.textContent = "Shooting Guard";
+            Small.appendChild(title);
+            Small.appendChild(name);
+            Small.appendChild(three);
+            Small.appendChild(two);
+            Small.appendChild(points);
+            break;
+        }
+        case "PF": {
+            Power.innerHTML = "";
+            title.textContent = "Power Forward";
+            Power.appendChild(title);
+            Power.appendChild(name);
+            Power.appendChild(three);
+            Power.appendChild(two);
+            Power.appendChild(points);
+            break;
+        }
+        case "C": {
+            Center.innerHTML = "";
+            title.textContent = "Center Forward";
+            Center.appendChild(title);
+            Center.appendChild(name);
+            Center.appendChild(three);
+            Center.appendChild(two);
+            Center.appendChild(points);
+            break;
+        }
+    }
+};
--- NBA.ts
+++ NBA.ts
@@ -6,6 +6,12 @@ const threePercent: HTMLSelectElement = document.querySelector('.three')!
 const points: HTMLSelectElement = document.querySelector('.points')!
 const Search: HTMLSelectElement = document.querySelector('.Search')!
 const Table: HTMLSelectElement = document.querySelector('.Table')!
+//
+const Point: HTMLSelectElement = document.querySelector('.Point')!
+const Shooting: HTMLSelectElement = document.querySelector('.Shooting')!
+const Small: HTMLSelectElement = document.querySelector('.Small')!
+const Power: HTMLSelectElement = document.querySelector('.Power')!
+const Center: HTMLSelectElement = document.querySelector('.Center')!
 
 
 
@@ -47,28 +53,32 @@ const addPlayer = async():Promise<void> => {
 
     // יצירת שורה
     let newRow = document.createElement("tr");
+    newRow.classList.add("prm");
     // יצירת אברים שיוצגו בשורה
-    let td1 = document.createElement("td");
-    let td2 = document.createElement("td");
-    let td3 = document.createElement("td");
-    let td4 = document.createElement("td");
-    let td5 = document.createElement("td");
-    let button = document.createComment("button")
-    td1.textContent = player.playerName;
-    td2.textContent = player.position;
-    td3.textContent = player.twePercent;
-    td4.textContent =player.threePercent;
-    button.textContent = `add${player.playerName} to Current Team`
+    let td1: HTMLElement  = document.createElement("td");
+    let td2: HTMLElement  = document.createElement("td");
+    let td3: HTMLElement  = document.createElement("td");
+    let td4: HTMLElement  = document.createElement("td");
+    let td5: HTMLElement  = document.createElement("td");
+    let td6: HTMLElement  = document.createElement("td");
+    let button: HTMLElement  = document.createElement("button")
+    button.classList.add("buttonDisplay")
+   
+    button.addEventListener("click",() => displayOnCubse(player))
+    td1.textContent = player.playerName
+    td2.textContent = player.position
+    td3.textContent =  player.points.toString()
+    td4.textContent =player.twoPercent.toString()
+    td5.textContent =player.threePercent.toString()
+    button.textContent = `add "${player.playerName}" to Current Team`
+    td6.appendChild(button)
     newRow.appendChild(td1)
     newRow.appendChild(td2)
     newRow.appendChild(td3)
     newRow.appendChild(td4)
+    newRow.appendChild(td5)
     newRow.appendChild(button)
     Table.appendChild(newRow)
-
-
-
-
  }
 
 
@@ -76,8 +86,78 @@ const addPlayer = async():Promise<void> => {
 
 
 
-
+//
 Search.addEventListener("click",addPlayer )
+
+const displayOnCubse = (player:Nba):void =>{
+    let title: HTMLElement  = document.createElement("h3");
+    title.style.color = "blue"
+    let name: HTMLElement  = document.createElement("h1");
+    let three : HTMLElement  = document.createElement("h3");
+    let two: HTMLElement  = document.createElement("h3");
+    let points: HTMLElement  = document.createElement("h3");
+    name.textContent = player.playerName
+    three.textContent = ` Three Precents: ${player.threePercent.toString()}%`
+    two.textContent = `Tow Precents: ${player.twoPercent.toString()}%`
+    points.textContent = `points: ${player.points.toString()}`
+
+
+
+    switch(player.position){
+        case "PG":{
+            Point.innerHTML = ""
+            title.textContent = "Point Guard"
+            Point.appendChild(title)
+            Point.appendChild(name)
+            Point.appendChild(three)
+            Point.appendChild(two)
+            Point.appendChild(points)
+            break;
+        }
+        case "SG":{
+            Shooting.innerHTML =""
+            title.textContent = "Shooting Guard"
+            Shooting.appendChild(title)
+            Shooting.appendChild(name)
+            Shooting.appendChild(three)
+            Shooting.appendChild(two)
+            Shooting.appendChild(points)
+            break;
+        }
+        case "SF":{
+            Small.innerHTML =""
+            title.textContent = "Shooting Guard"
+            Small.appendChild(title)
+            Small.appendChild(name)
+            Small.appendChild(three)
+            Small.appendChild(two)
+            Small.appendChild(points)
+            break;
+        }
+        case "PF":{
+            Power.innerHTML =""
+            title.textContent = "Power Forward"
+            Power.appendChild(title)
+            Power.appendChild(name)
+            Power.appendChild(three)
+            Power.appendChild(two)
+            Power.appendChild(points)
+            break;  
+    }
+    case "C":{
+        Center.innerHTML =""
+        title.textContent = "Center Forward"
+        Center.appendChild(title)
+        Center.appendChild(name)
+        Center.appendChild(three)
+        Center.appendChild(two)
+        Center.appendChild(points)
+        break;
+    }  
+}
+
+
+}
 
 
 
@@ -91,8 +171,8 @@ Search.addEventListener("click",addPlayer )
 
 interface Nba {
     position:string,
-    twePercent:string,
-    threePercent:string,
-    points:string,
+    twoPercent:number,
+    threePercent:number,
+    points:number
     playerName:string
 }
